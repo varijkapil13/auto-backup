fba7472660a24649458f819c7c1360834b88f02d
Varij Kapil <varij.kapil@it-manufactory.com>
2022-10-14 08:52:37 +0200

    logs: added logging to page

--- models/Server.py
+++ models/Server.py
@@ -1,6 +1,16 @@
+import logging
+
 class Server:
     def __init__(self, url=None, port=None, db_name=None, username=None, password=None, server_name=None,
                  database_type="postgres"):
+        log = logging.getLogger()
+        log.setLevel(logging.INFO)
+
+        log.info("Hosts: " + url)
+        log.info("Ports: " + str(port))
+        log.info("Database Name: " + db_name)
+        log.info("Database Type: " + database_type)
+
         self.connection_string = "host='" + url + "' port='" + str(
                 port) + "' dbname='" + db_name + "' user='" + username + "' password='" + password + "'" \
                                  + "' Database Type='" + database_type + "'"
